5ab45b60a896174147bc62b84dd7ba46605ca9d4
Mauricio Rohling <mauricio.uto@hotmail.com>
2024-02-06 13:57:02 -0300

    Add project files.

---
+++ SuperHeroAPI/Entities/SuperHero.cs
@@ -0,0 +1,12 @@
+﻿namespace SuperHeroAPI.Entities
+{
+    public class SuperHero
+    {
+        public int Id { get; set; }
+        public required string Name { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string Place { get; set; }
+
+    }
+}
